0a206b432b297bacd5f76d1119d90cf75a34038a
ALX <3496916+alexplong@users.noreply.github.com>
2021-07-26 10:36:45 -0700

    MacBook push

--- sandbox.js
+++ sandbox.js
@@ -33,6 +33,7 @@ grids.forEach(grid =>{
 
 // Reset button to reset the grid back to original color and to ask user for new input for new grid size
 const resetButton = document.getElementById('resetButton');
+const randomColorButton = document.getElementById('randomColorButton');
 const rainbowButton = document.getElementById('rainbowButton');
 const gridInput = document.getElementById('gridInput');
 
@@ -63,9 +64,11 @@ resetButton.addEventListener('click', () =>{
     });
 });
 
-     
+function randomNumber(num){
+    return Math.floor(Math.random() * num);
+}
 
-rainbowButton.addEventListener('click', () =>{
+randomColorButton.addEventListener('click', () =>{
     let x = gridInput.value;
     documentBody.removeChild(container);
     container = document.createElement('div');
@@ -87,12 +90,93 @@ rainbowButton.addEventListener('click', () =>{
     newGrids.forEach(grid =>{
         grid.addEventListener('mouseover', () =>{
             grid.removeAttribute('class');
-            setAttribute('style', `background-color: ${colour}`);
+            grid.setAttribute('style', `background-color: rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)}`);
         });
     });
 });
 
 
+
+var frequency = .3;
+function rgbRandom(){
+    for (d = 0; d < 32; d++){
+       red   = Math.sin(frequency*d + 0) * 127 + 128;
+       green = Math.sin(frequency*d + 2) * 127 + 128;
+       blue  = Math.sin(frequency*d + 4) * 127 + 128;
+       colours = `${red},  ${green}, ${blue}`;
+       console.log(d);
+       return colours;
+    //    document.write( '<font color="' + RGB2Color(red,green,blue) + '">&#9608;</font>');
+    }
+}
+
+rainbowButton.addEventListener('click', () =>{
+        let x = gridInput.value;
+        documentBody.removeChild(container);
+        container = document.createElement('div');
+        container.setAttribute('id', 'main-body');
+        documentBody.appendChild(container);
+        container.setAttribute('style', `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr)`);
+        let j = 0;
+        for (j = 0; j < x*x; j++){
+            if (x > 100) {
+                return alert('Whoa! That\'s too big and resources will kaput. Enter a number < 100 =]');
+                break;    
+            };
+            let newGrid = document.createElement('div');
+            newGrid.setAttribute('class', 'grids');
+            container.appendChild(newGrid);
+        };
+        let newGridBody = document.getElementsByClassName('grids');
+        let newGrids = Array.from(newGridBody);
+        newGrids.forEach(grid =>{
+            grid.addEventListener('mouseover', () =>{
+                grid.removeAttribute('class');
+                grid.setAttribute('style', `background-color: rgb(${rgbRandom()})`);
+                })
+            })
+        }
+);
+
+
+// rainbowButton.addEventListener('click', () =>{
+//     let x = gridInput.value;
+//     documentBody.removeChild(container);
+//     container = document.createElement('div');
+//     container.setAttribute('id', 'main-body');
+//     documentBody.appendChild(container);
+//     container.setAttribute('style', `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr)`);
+//     let j = 0;
+//     for (j = 0; j < x*x; j++){
+//         if (x > 100) {
+//             return alert('Whoa! That\'s too big and resources will kaput. Enter a number < 100 =]');
+//             break;    
+//         };
+//         let newGrid = document.createElement('div');
+//         newGrid.setAttribute('class', 'grids');
+//         container.appendChild(newGrid);
+//     };
+//     let newGridBody = document.getElementsByClassName('grids');
+//     let newGrids = Array.from(newGridBody);
+//     newGrids.forEach(grid =>{
+//         grid.addEventListener('mouseover', () =>{
+//             grid.removeAttribute('class');
+//             grid.setAttribute('style', `background-color: rgb(${rgbRandom()})`);
+//         });
+//     });
+// });
+
+
+
+// function rgbRandom(){
+//     for( l=0; l < 1<<24; l++) {
+//         r = (l>>16) & 0xff;
+//         g = (l>>8) & 0xff;
+//         b = l & 0xff;
+//         colour = "rgb("+r+","+g+","+b+")";
+//     };
+//     return colour;
+// };     
 
 
 // rainbow loop
