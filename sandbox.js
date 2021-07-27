b8ba2b041b0d22930821d8396047a5cb268213ea
ALX <3496916+alexplong@users.noreply.github.com>
2021-07-27 10:36:11 -0700

    Cleaned up code.

--- sandbox.js
+++ sandbox.js
@@ -4,24 +4,14 @@ let container = document.createElement('div');
 container.setAttribute('id', 'main-body');
 documentBody.appendChild(container);
 
-// create a default 16x16 grid of square divs
-
-// while loop edition
+// create default 16x16 grid of divs
 let i = 0;
-// while (i < 16) {
-//     let newGrid = document.createElement('div');
-//     newGrid.setAttribute('class', 'grids');
-//     container.appendChild(newGrid);
-//     i++;
-// }
-// for loop version of above
 for (i = 0; i < 16*16; i++){
     let newGrid = document.createElement('div');
     newGrid.setAttribute('class', 'grids');
     container.appendChild(newGrid);
 };
 
-// const gridBody = document.querySelectorAll('div' > 'grids'); // one way of doing it
 const gridBody = document.getElementsByClassName('grids'); 
 let grids = Array.from(gridBody); // HTML list unable to iterate so need to convert to Array
 grids.forEach(grid =>{
@@ -30,13 +20,10 @@ grids.forEach(grid =>{
     });
 });
  
-
 // Reset button to reset the grid back to original color and to ask user for new input for new grid size
 const resetButton = document.getElementById('resetButton');
-const randomColorButton = document.getElementById('randomColorButton');
 const rainbowButton = document.getElementById('rainbowButton');
 const gridInput = document.getElementById('gridInput');
-
 
 resetButton.addEventListener('click', () =>{
     let x = gridInput.value;
@@ -64,53 +51,8 @@ resetButton.addEventListener('click', () =>{
     });
 });
 
-function randomNumber(num){
-    return Math.floor(Math.random() * num);
-}
 
-randomColorButton.addEventListener('click', () =>{
-    let x = gridInput.value;
-    documentBody.removeChild(container);
-    container = document.createElement('div');
-    container.setAttribute('id', 'main-body');
-    documentBody.appendChild(container);
-    container.setAttribute('style', `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr)`);
-    let j = 0;
-    for (j = 0; j < x*x; j++){
-        if (x > 100) {
-            return alert('Whoa! That\'s too big and resources will kaput. Enter a number < 100 =]');
-            break;    
-        };
-        let newGrid = document.createElement('div');
-        newGrid.setAttribute('class', 'grids');
-        container.appendChild(newGrid);
-    };
-    let newGridBody = document.getElementsByClassName('grids');
-    let newGrids = Array.from(newGridBody);
-    newGrids.forEach(grid =>{
-        grid.addEventListener('mouseover', () =>{
-            grid.removeAttribute('class');
-            grid.setAttribute('style', `background-color: rgb(${randomNumber(255)}, ${randomNumber(255)}, ${randomNumber(255)}`);
-        });
-    });
-});
-
-
-
-// function rgbRandom(){
-//     for (d = 0; d < 32; d++){
-//         let frequency = .3;
-//         red   = Math.sin(frequency*d + 0) * 127 + 128;
-//         green = Math.sin(frequency*d + 2) * 127 + 128;
-//         blue  = Math.sin(frequency*d + 4) * 127 + 128;
-//         colours = `${red},  ${green}, ${blue}`;
-//         console.log(d);
-//         console.log(colours);
-//         return colours;
-//     }
-// };
-
-// fixed rainbow function by changing to a while loop
+// rainbow function loop
 let d = 1; 
 function rgbRandom(){
     while (d != 0){
@@ -125,7 +67,6 @@ function rgbRandom(){
         return colours;
     } 
 };
-
 rainbowButton.addEventListener('click', () =>{
         let x = gridInput.value;
         documentBody.removeChild(container);
@@ -153,60 +94,3 @@ rainbowButton.addEventListener('click', () =>{
         })
     }
 );
-
-
-// rainbowButton.addEventListener('click', () =>{
-//     let x = gridInput.value;
-//     documentBody.removeChild(container);
-//     container = document.createElement('div');
-//     container.setAttribute('id', 'main-body');
-//     documentBody.appendChild(container);
-//     container.setAttribute('style', `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr)`);
-//     let j = 0;
-//     for (j = 0; j < x*x; j++){
-//         if (x > 100) {
-//             return alert('Whoa! That\'s too big and resources will kaput. Enter a number < 100 =]');
-//             break;    
-//         };
-//         let newGrid = document.createElement('div');
-//         newGrid.setAttribute('class', 'grids');
-//         container.appendChild(newGrid);
-//     };
-//     let newGridBody = document.getElementsByClassName('grids');
-//     let newGrids = Array.from(newGridBody);
-//     newGrids.forEach(grid =>{
-//         grid.addEventListener('mouseover', () =>{
-//             grid.removeAttribute('class');
-//             grid.setAttribute('style', `background-color: rgb(${rgbRandom()})`);
-//         });
-//     });
-// });
-
-
-
-// function rgbRandom(){
-//     for( l=0; l < 1<<24; l++) {
-//         r = (l>>16) & 0xff;
-//         g = (l>>8) & 0xff;
-//         b = l & 0xff;
-//         colour = "rgb("+r+","+g+","+b+")";
-//     };
-//     return colour;
-// };     
-
-
-// rainbow loop
-// var red, green, blue;
-// for (red = 0; red <= 255; red++){
-//     for (green = 0; green <= 255; green++){
-//         for (blue = 0; blue <= 255; blue++){
-//             //rgb(red, green blue)
-//         }
-//     }
-// }
-// for( l=0; l < 1<<24; l++) {
-//     r = (l>>16) & 0xff;
-//     g = (l>>8) & 0xff;
-//     b = l & 0xff;
-//     colour = "rgb("+r+","+g+","+b+")";
-// };       
