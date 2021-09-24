ab4e8d6f064ca9c1512a1f6a0f972e88e248d796
ALX <3496916+alexplong@users.noreply.github.com>
2021-09-24 06:39:06 -0700

    git upload

--- sandbox.js
+++ sandbox.js
@@ -25,9 +25,48 @@ container.addEventListener('mouseover', (e) => {
 const resetButton = document.getElementById('resetButton');
 const rainbowButton = document.getElementById('rainbowButton');
 
-// Updated reset button
+// Update to slider button and display current value
+const slider = document.getElementById('mySlider');
+const output = document.getElementById('demo');
+output.innerHTML = slider.value;
+
+// Update current slider value each time you drag the slider
+slider.oninput = function() {
+    output.innerHTML = this.value;
+    console.log(this.value);
+};
+
+
+// // Updated reset button
+// resetButton.addEventListener('click', () =>{
+//     let x = gridInput.value;
+//     documentBody.removeChild(container);
+//     container = document.createElement('div');
+//     container.setAttribute('id', 'main-body');
+//     documentBody.appendChild(container);
+//     container.setAttribute('style', `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr)`);
+//     let j = 0;
+//     for (j = 0; j < x*x; j++){
+//         if (x > 100) {
+//             return alert('Whoa! That\'s too big and resources will kaput. Enter a number < 100 =]');
+//             break;    
+//         };
+//         let newGrid = document.createElement('div');
+//         newGrid.setAttribute('class', 'grids');
+//         container.appendChild(newGrid);
+//     };
+//     container.addEventListener('mouseover', (e) => {
+//         if (e.target.className === 'grids'){
+//             e.target.setAttribute('class', 'hovering');
+//         } else {
+//             // do nothing
+//         }
+//     })
+// });
+
+// Update to use slider input
 resetButton.addEventListener('click', () =>{
-    let x = gridInput.value;
+    let x = mySlider.value;
     documentBody.removeChild(container);
     container = document.createElement('div');
     container.setAttribute('id', 'main-body');
@@ -52,7 +91,49 @@ resetButton.addEventListener('click', () =>{
     })
 });
 
+
 // Updated rainbow function
+// let d = 1; 
+// function rgbRandom(){
+//     while (d != 0){
+//         let frequency = .3;
+//         red   = Math.sin(frequency*d + 0) * 127 + 128;
+//         green = Math.sin(frequency*d + 2) * 127 + 128;
+//         blue  = Math.sin(frequency*d + 4) * 127 + 128;
+//         colours = `${red},  ${green}, ${blue}`;
+//         d++;
+//         return colours;
+//     } 
+// };
+// rainbowButton.addEventListener('click', () =>{
+//         let x = gridInput.value;
+//         documentBody.removeChild(container);
+//         container = document.createElement('div');
+//         container.setAttribute('id', 'main-body');
+//         documentBody.appendChild(container);
+//         container.setAttribute('style', `display: grid; grid-template-columns: repeat(${x}, 1fr); grid-template-rows: repeat(${x}, 1fr)`);
+//         let j = 0;
+//         for (j = 0; j < x*x; j++){
+//             if (x > 100) {
+//                 return alert('Whoa! That\'s too big and resources will kaput. Enter a number < 100 =]');
+//                 break;    
+//             };
+//             let newGrid = document.createElement('div');
+//             newGrid.setAttribute('class', 'grids');
+//             container.appendChild(newGrid);
+//         };
+//         container.addEventListener('mouseover', (e) => {
+//             if (e.target.className === 'grids') {
+//                 // do this
+//                 e.target.setAttribute('style', `background-color: rgb(${rgbRandom()})`);
+//             } else {
+//                 // do nothing
+//             }
+//         })
+//     }
+// );
+
+// Updated rainbow slider
 let d = 1; 
 function rgbRandom(){
     while (d != 0){
@@ -66,7 +147,7 @@ function rgbRandom(){
     } 
 };
 rainbowButton.addEventListener('click', () =>{
-        let x = gridInput.value;
+        let x = mySlider.value;
         documentBody.removeChild(container);
         container = document.createElement('div');
         container.setAttribute('id', 'main-body');
@@ -92,5 +173,4 @@ rainbowButton.addEventListener('click', () =>{
         })
     }
 );
-
     
